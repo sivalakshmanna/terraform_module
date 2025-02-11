109b29c657160fa69f5fbf9b8162f0ebc3a03a48
sivalakshmanna <94002684+sivalakshmanna@users.noreply.github.com>
2025-02-11 12:11:44 +0530

    change

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
+resource "aws_subnet" "subnet" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.subnet_cidr
+  availability_zone       = "ap-south-1a"  # Choose the availability zone
+  map_public_ip_on_launch = true
+}
--- variables.tf
+++ variables.tf
@@ -5,3 +5,7 @@ variable "access_key" {
 variable "secret_key" {
     
 }
+
+variable "subnet_cidr" {
+    
+}
